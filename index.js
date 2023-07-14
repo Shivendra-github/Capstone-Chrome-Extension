3721899072ac0624326a6ddedb1df931b1e1e0cb
Shivendra-github <shivendrapratapsingh96178@gmail.com>
2023-07-14 17:57:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+    .then(res => res.json())
+    .then(data =>{
+
+
+        document.body.style.backgroundImage= `url(${data.urls.full})`
+       
+        document.getElementById('author').innerText=`By: ${data.user.name}`
+        
+
+
+    });
