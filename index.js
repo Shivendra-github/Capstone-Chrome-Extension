e602001dcf214e6faa3efa5c245c7ee8337dd50f
Shivendra-github <shivendrapratapsingh96178@gmail.com>
2023-07-17 12:43:27 +0530

    weather api
    weather api implimentation.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let backUpImg = ['https://images.unsplash.com/photo-1514521068412-14c7cd29cc23?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80',''];
 
-fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature')
+fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=forest')
     .then(res => res.json())
     .then(data =>{
 
@@ -46,9 +46,9 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
             
           
 
-            <p>🎯&nbsp&nbsp$${data.market_data.current_price.usd}</p>
-            <p>📈&nbsp&nbsp$${data.market_data.high_24h.usd}</p>
-            <p>📉&nbsp&nbsp$${data.market_data.low_24h.usd}</p>
+            <p>🎯:&nbsp&nbsp$${data.market_data.current_price.usd}</p>
+            <p>📈:&nbsp&nbsp$${data.market_data.high_24h.usd}</p>
+            <p>📉:&nbsp&nbsp$${data.market_data.low_24h.usd}</p>
 
             
             
@@ -65,25 +65,28 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
         function Time(){
 
             var today = new Date();
-            var time = today.getHours() + ":" + today.getMinutes();
             var hours = today.getHours();
+
+            var date = new Date();
+            
+            let time = date.toLocaleTimeString('en-GB',{timeStyle:"short"})
 
             document.getElementById('time').innerHTML= `<h1>${time}<h1>`
             
            
             
+         
 
-
-            if(hours>=1 && hours<=12){
+            if(time>='1:00' && time<='12:00'){
                 document.getElementById('greet').innerHTML=`<h2>Good Morning</h2>`
                 
-            }else if(hours>=12 && hours<=16){
+            }else if(time>='12:00' && time<='16:00'){
                
                 document.getElementById('greet').innerHTML=`<h2>Good Aftermoon</h2>`
-            }else if(hours>=16 && hours<=21){
+            }else if(time>='16:00' && time<='21:00'){
              
                 document.getElementById('greet').innerHTML=`<h2>Good Evening</h2>`
-            }else if(hours>=21 && hours<=24){
+            }else if(time>='21:00' && time<='24:00'){
                
                 document.getElementById('greet').innerHTML=`<h2>Good Night</h2>`
             }
@@ -94,6 +97,31 @@ fetch('https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&que
 
         }
         
+// Weather API
+
+navigator.geolocation.getCurrentPosition(position => {
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok) {
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+          const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+          document.getElementById("Weather").innerHTML = `
+            <div id="degree">
+              <img src=${iconUrl} />
+              <p class="weather-temp">${Math.round(data.main.temp)}º</p>
+          </div>
+              <p class="weather-city">${data.name}</p>
+          `
+          console.log(data);
+      })
+      .catch(err => console.error(err))
+});
+
+
 
 
         
